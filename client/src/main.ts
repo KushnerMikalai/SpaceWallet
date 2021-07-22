2ffc8bf5e4ecc7ab5d51fa9c79736f93395030f6
nikolai.k <nikolai.k@seranking.com>
2021-07-22 09:17:18 +0300

    client npm update

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,10 +2,8 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import {store, key} from './store'
 import router from './router'
-import {VueCookieNext} from 'vue-cookie-next'
 
 const app = createApp(App)
 app.use(store, key)
 app.use(router)
-app.use(VueCookieNext)
 app.mount('#app')
