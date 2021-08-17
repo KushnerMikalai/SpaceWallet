da035b29543ab9eba5adff37121b4ba8b51d7f0b
KushnerMikalai <nickolaikushner@gmail.com>
2021-08-17 22:41:29 +0300

    smtp to env

--- server/helpers/smpt.ts
+++ server/helpers/smpt.ts
@@ -4,8 +4,8 @@ import { config } from "./../config/config.ts";
 const client = new SmtpClient();
 
 await client.connectTLS({
-  hostname: "smtp.yandex.ru",
-  port: 465,
+  hostname: config.SMTP_HOSTNAME,
+  port: +config.SMTP_PORT,
   username: config.SMTP_USERNAME,
   password: config.SMTP_PASSWORD,
 });
