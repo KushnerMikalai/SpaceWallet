ae073162e1ffff15e41573c036c68822ccc3fbf4
KushnerMikalai <nickolaikushner@gmail.com>
2021-09-01 09:27:15 +0300

    solid

---
+++ solid/src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return (
+    <div class={styles.app}>
+        <header class={styles.header}>x</header>
+    </div>
+  );
+};
+
+export default App;
---
+++ solid/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  build: {
+    target: "esnext",
+    polyfillDynamicImport: false,
+  },
+  server: {
+    proxy: {
+      // TODO DENO server
+      '/api': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    }
+  }
+});
